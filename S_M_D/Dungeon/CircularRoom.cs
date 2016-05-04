a7732f42e5185ce48f4ac0f03b59d93ec839e60f
Benoit Humblot <benoit.humblot@orange.fr>
2016-05-04 10:57:20 +0200

    Implementation de la méthode pointIsInsideRoom de la classe CircularRoom

--- S_M_D/Dungeon/CircularRoom.cs
+++ S_M_D/Dungeon/CircularRoom.cs
@@ -3,24 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
 
 namespace S_M_D.Dungeon
 {
     class CircularRoom : Room
     {
-        public override bool canPlaceRoom(MapItem[,] grid)
-        {
-            throw new NotImplementedException();
-        }
+        private int radius;
 
-        public override void placeRoom(MapItem[,] grid)
-        {
-            throw new NotImplementedException();
-        }
-
+        /// <summary>
+        /// Use Pythgora to determine if the point is within the circular room
+        /// </summary>
+        /// <param name="x"> X coordinate of the input point </param>
+        /// <param name="y"> Y coordinate of the input point </param>
+        /// <returns></returns>
         public override bool pointIsInsideRoom(int x, int y)
         {
-            throw new NotImplementedException();
+            return Math.Pow((x - path[0].X), 2) + Math.Pow((y - path[0].Y), 2) <= Math.Pow(radius, 2) ? true : false;
         }
     }
 }
