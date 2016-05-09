2d7cdf60b8d5a59723a6f8b249a4b7ac8f8ca510
synouk <crousseau@intechinfo.fr>
2016-05-09 14:50:01 +0200

    ItemCreator

---
+++ ItemCreator/ItemCreator/BaseItem.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItemCreator
+{
+    [Serializable]
+    public class BaseItem
+    {
+        string itemName;
+        string itemDescription;
+        int itemId;
+        public enum ItemTypes
+        {
+            Trinket,
+            Potion
+        }
+        ItemTypes itemtype;
+
+        public string ItemName
+        {
+            get
+            {
+                return itemName;
+            }
+
+            set
+            {
+                itemName = value;
+            }
+        }
+
+        public string ItemDescription
+        {
+            get
+            {
+                return itemDescription;
+            }
+
+            set
+            {
+                itemDescription = value;
+            }
+        }
+
+        public int ItemId
+        {
+            get
+            {
+                return itemId;
+            }
+
+            set
+            {
+                itemId = value;
+            }
+        }
+
+        public ItemTypes Itemtype
+        {
+            get
+            {
+                return itemtype;
+            }
+
+            set
+            {
+                itemtype = value;
+            }
+        }
+    }
+}
