9f50e6be0bd5f0dc2580d0f6d6d49671ad06662b
Peuch <lucas.78.13@gmail.com>
2016-05-09 16:55:08 +0200

    CaravanConfig

---
+++ S_M_D/Camp/ClassConfig/CaravanConfig.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using S_M_D.Character;
+namespace S_M_D.Camp.ClassConfig
+{
+    public class CaravanConfig : BuildingType
+    {
+        List<BaseHeros> herosDispo;
+        public CaravanConfig(string name, int buildingCost, List<BaseBuilding> buildings,List<BaseHeros> heros) : base(name, buildingCost, buildings)
+        {
+            this.herosDispo = heros;
+        }
+    }
+}
