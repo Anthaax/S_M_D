fea0b76005a9f7f26098679714a1fd2d3a522609
Peuch <lucas.78.13@gmail.com>
2016-05-10 13:40:59 +0200

    TownHallConfig

--- S_M_D/Camp/ClassConfig/TownHallConfig.cs
+++ S_M_D/Camp/ClassConfig/TownHallConfig.cs
@@ -8,7 +8,6 @@ namespace S_M_D.Camp.ClassConfig
 {
     public class TownHallConfig : BuildingType
     {
-
         public TownHallConfig(string name, int buildingCost, List<BaseBuilding> buildings) : base(name, buildingCost, buildings)
         {
 
