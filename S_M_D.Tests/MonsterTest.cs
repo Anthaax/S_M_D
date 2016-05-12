20e81801f3194e94677195a98411bfd30625ffeb
Peuch <lucas.78.13@gmail.com>
2016-05-12 09:33:18 +0200

    Monstres Creations test

--- S_M_D.Tests/MonsterTest.cs
+++ S_M_D.Tests/MonsterTest.cs
@@ -38,7 +38,81 @@ namespace S_M_D.Tests
             Assert.AreEqual( 2, orcMonster.GiveXp );
             Assert.AreEqual( MonsterType.ORC, orcMonster.Type );
         }
+        [Test]
+        public void CreationElfTest()
+        {
+            MonsterConfiguration config = new MonsterConfiguration();
+            BaseMonster orcMonster = config.CreateMonster(MonsterType.ELF, 2);
 
+            Assert.AreEqual(24, orcMonster.HPmax);
+            Assert.AreEqual(24, orcMonster.HP);
+            Assert.AreEqual(50, orcMonster.ManaMax);
+            Assert.AreEqual(50, orcMonster.Mana);
+            Assert.AreEqual(12, orcMonster.Damage);
+            Assert.AreEqual(0, orcMonster.CritChance);
+            Assert.AreEqual(80, orcMonster.HitChance);
+            Assert.AreEqual(10, orcMonster.Speed);
+            Assert.AreEqual(0, orcMonster.AffectRes);
+            Assert.AreEqual(0, orcMonster.BleedingRes);
+            Assert.AreEqual(0, orcMonster.MagicRes);
+            Assert.AreEqual(0, orcMonster.FireRes);
+            Assert.AreEqual(0, orcMonster.PoisonRes);
+            Assert.AreEqual(0, orcMonster.WaterRes);
+            Assert.AreEqual(0, orcMonster.Defense);
+            Assert.AreEqual(40, orcMonster.DodgeChance);
+            Assert.AreEqual(2, orcMonster.GiveXp);
+            Assert.AreEqual(MonsterType.ELF, orcMonster.Type);
+        }
+        [Test]
+        public void CreationGobelincTest()
+        {
+            MonsterConfiguration config = new MonsterConfiguration();
+            BaseMonster orcMonster = config.CreateMonster(MonsterType.GOBELIN, 1);
+
+            Assert.AreEqual(21, orcMonster.HPmax);
+            Assert.AreEqual(21, orcMonster.HP);
+            Assert.AreEqual(45, orcMonster.ManaMax);
+            Assert.AreEqual(45, orcMonster.Mana);
+            Assert.AreEqual(4, orcMonster.Damage);
+            Assert.AreEqual(0, orcMonster.CritChance);
+            Assert.AreEqual(80, orcMonster.HitChance);
+            Assert.AreEqual(10, orcMonster.Speed);
+            Assert.AreEqual(0, orcMonster.AffectRes);
+            Assert.AreEqual(0, orcMonster.BleedingRes);
+            Assert.AreEqual(0, orcMonster.MagicRes);
+            Assert.AreEqual(0, orcMonster.FireRes);
+            Assert.AreEqual(0, orcMonster.PoisonRes);
+            Assert.AreEqual(0, orcMonster.WaterRes);
+            Assert.AreEqual(0, orcMonster.Defense);
+            Assert.AreEqual(60, orcMonster.DodgeChance);
+            Assert.AreEqual(1, orcMonster.GiveXp);
+            Assert.AreEqual(MonsterType.GOBELIN, orcMonster.Type);
+        }
+        [Test]
+        public void CreationTrollTest()
+        {
+            MonsterConfiguration config = new MonsterConfiguration();
+            BaseMonster orcMonster = config.CreateMonster(MonsterType.TROLL, 1);
+
+            Assert.AreEqual(28, orcMonster.HPmax);
+            Assert.AreEqual(28, orcMonster.HP);
+            Assert.AreEqual(43, orcMonster.ManaMax);
+            Assert.AreEqual(43, orcMonster.Mana);
+            Assert.AreEqual(12, orcMonster.Damage);
+            Assert.AreEqual(30, orcMonster.CritChance);
+            Assert.AreEqual(70, orcMonster.HitChance);
+            Assert.AreEqual(5, orcMonster.Speed);
+            Assert.AreEqual(0, orcMonster.AffectRes);
+            Assert.AreEqual(0, orcMonster.BleedingRes);
+            Assert.AreEqual(0, orcMonster.MagicRes);
+            Assert.AreEqual(0, orcMonster.FireRes);
+            Assert.AreEqual(0, orcMonster.PoisonRes);
+            Assert.AreEqual(0, orcMonster.WaterRes);
+            Assert.AreEqual(0, orcMonster.Defense);
+            Assert.AreEqual(10, orcMonster.DodgeChance);
+            Assert.AreEqual(8, orcMonster.GiveXp);
+            Assert.AreEqual(MonsterType.TROLL, orcMonster.Type);
+        }
         [Test]
         public void DeadMonsterTest()
         {
