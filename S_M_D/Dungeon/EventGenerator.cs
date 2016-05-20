0a65473dbf8b75a0befda4fc05c3e37c49fb4b47
Benoit Humblot <benoit.humblot@orange.fr>
2016-05-20 10:35:20 +0200

    fusoin avec la branche develop
    Implémentation de la méthode FillChest

--- S_M_D/Dungeon/EventGenerator.cs
+++ S_M_D/Dungeon/EventGenerator.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using ItemCreator;
+using System.IO;
+using System.Xml.Serialization;
+using S_M_D.Character;
 
 namespace S_M_D.Dungeon
 {
@@ -21,7 +25,35 @@ namespace S_M_D.Dungeon
 
         private void FillChest( )
         {
+            Random rand = new Random( );
+            int item = rand.Next( 200 );
+            string path;
+            Character.BaseItem result;
 
+            int selection = rand.Next( 1, 2 );
+
+            if(selection == 1)
+            {
+                path = "Items/Armors.xml";
+                using ( FileStream myFileStream = new FileStream( path, FileMode.Open ) )
+                {
+                    XmlSerializer reader = new XmlSerializer( typeof( List<Character.BaseArmor> ) );
+                    List<Character.BaseArmor> overview = ( List<Character.BaseArmor> ) reader.Deserialize( myFileStream );
+                    result = overview[ item ];
+
+                }
+            }
+            else
+            {
+                path = "Items/Weapons.xml";
+                using ( FileStream myFileStream = new FileStream( path, FileMode.Open ) )
+                {
+                    XmlSerializer reader = new XmlSerializer( typeof( List<Character.BaseWeapon> ) );
+                    List<Character.BaseWeapon> overview = ( List<Character.BaseWeapon> ) reader.Deserialize( myFileStream );
+                    result = overview[ item ];
+
+                }
+            }
         }
 
         public void Generate(Map map)
